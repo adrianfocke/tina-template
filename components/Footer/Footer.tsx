fde402417c1803a4bbb6a5212ac668315899a8bb
Focke Adrian <afocke@pm.me>
2026-02-24 11:21:17 +0100

    changes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Flex, Select } from "@radix-ui/themes";
+import { Box, Container, Flex, Select } from "@radix-ui/themes";
 import { useContext } from "react";
 import { LanguageContext } from "../../utils/context/language";
 import { languages, type Language } from "../../tina/templating/special-fields";
@@ -24,49 +24,59 @@ export default function Footer(props: FooterQuery["footer"]) {
 
   return (
     <Box
-      style={{ borderTop: "1px solid var(--gray-6)" }}
+      style={{
+        borderTop: "1px solid var(--gray-6)",
+        marginTop: "var(--space-6)",
+      }}
       mx={props.settings?.marginX ?? "0"}
       mt={props.settings?.marginY ?? "0"}
       px={props.settings?.paddingX ?? "0"}
       py={props.settings?.paddingY ?? "0"}
     >
-      <Flex justify={"between"} align={"center"}>
-        <Flex gap={"4"} direction={"row"} display={"flex"} align={"center"}>
-          {props.links?.map((link, index) => {
-            return (
-              <Text
-                key={index}
-                {...(link as any)}
-                style={{ background: "red", width: "20px" }}
-              />
-            );
-          })}
-          <Button
-            content={{
-              text_de: "Signal",
-              text_en: "Signal",
-            }}
-            settings={{ align: "center", variant: "outline" }}
-          />
-          <Button
-            content={{
-              text_de: "WhatsApp",
-              text_en: "WhatsApp",
-            }}
-            settings={{ align: "center", variant: "outline" }}
-          />
+      <Container>
+        <Flex justify={"between"} align={"center"}>
+          <Flex gap={"4"} direction={"row"} display={"flex"} align={"center"}>
+            {props.links?.map((link, index) => {
+              return (
+                <Text
+                  key={index}
+                  {...(link as any)}
+                  style={{ background: "red", width: "20px" }}
+                />
+              );
+            })}
+            <Button
+              content={{
+                text_de: "Signal",
+                text_en: "Signal",
+              }}
+              settings={{ align: "center", variant: "outline", textSize: "2" }}
+            />
+            <Button
+              content={{
+                text_de: "WhatsApp",
+                text_en: "WhatsApp",
+              }}
+              settings={{ align: "center", variant: "outline", textSize: "2" }}
+            />
+          </Flex>
+          <Select.Root value={language} onValueChange={handleLanguageChange}>
+            <Select.Trigger
+              aria-label="Select language"
+              style={{
+                borderRadius: "var(--radius-6)",
+              }}
+            />
+            <Select.Content>
+              {languages.map((lang) => (
+                <Select.Item key={lang} value={lang}>
+                  {languageLabels[lang]}
+                </Select.Item>
+              ))}
+            </Select.Content>
+          </Select.Root>
         </Flex>
-        <Select.Root value={language} onValueChange={handleLanguageChange}>
-          <Select.Trigger aria-label="Select language" />
-          <Select.Content>
-            {languages.map((lang) => (
-              <Select.Item key={lang} value={lang}>
-                {languageLabels[lang]}
-              </Select.Item>
-            ))}
-          </Select.Content>
-        </Select.Root>
-      </Flex>
+      </Container>
     </Box>
   );
 }
--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,4 +1,10 @@
-import { Flex, Box, Popover, Button as RadixButton } from "@radix-ui/themes";
+import {
+  Flex,
+  Box,
+  Popover,
+  Button as RadixButton,
+  Container,
+} from "@radix-ui/themes";
 import type { NavigationQuery } from "../../tina/__generated__/types";
 import Text from "../Text/Text";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
@@ -12,50 +18,52 @@ export default function Navigation(props: NavigationQuery["navigation"]) {
       px={props.settings?.paddingX ?? "0"}
       py={props.settings?.paddingY ?? "0"}
     >
-      <Flex direction={"row"} justify="between" align={"center"}>
-        <Box>
-          <Text {...(props.logo as any)} />
-        </Box>
+      <Container>
+        <Flex direction={"row"} justify="between" align={"center"}>
+          <Box>
+            <Text {...(props.logo as any)} />
+          </Box>
 
-        <Box display={{ initial: "block", md: "none" }} mr={"4"}>
-          <Popover.Root>
-            <Popover.Trigger>
-              <RadixButton
-                variant="ghost"
-                size="4"
-                mt={"1"}
-                aria-label="Open navigation menu"
+          <Box display={{ initial: "block", md: "none" }} mr={"4"}>
+            <Popover.Root>
+              <Popover.Trigger>
+                <RadixButton
+                  variant="ghost"
+                  size="4"
+                  mt={"1"}
+                  aria-label="Open navigation menu"
+                >
+                  <HamburgerMenuIcon />
+                </RadixButton>
+              </Popover.Trigger>
+              <Popover.Content
+                style={{
+                  width: "fit-content",
+                  background: "var(--color-background)",
+                }}
+                side="bottom"
+                align="end"
               >
-                <HamburgerMenuIcon />
-              </RadixButton>
-            </Popover.Trigger>
-            <Popover.Content
-              style={{
-                width: "fit-content",
-                background: "var(--color-background)",
-              }}
-              side="bottom"
-              align="end"
-            >
-              <Flex direction="column" gap="3">
-                {props.links?.map((link, index) => {
-                  return <Text key={index} {...(link as any)} />;
-                })}
-              </Flex>
-            </Popover.Content>
-          </Popover.Root>
-        </Box>
+                <Flex direction="column" gap="3">
+                  {props.links?.map((link, index) => {
+                    return <Text key={index} {...(link as any)} />;
+                  })}
+                </Flex>
+              </Popover.Content>
+            </Popover.Root>
+          </Box>
 
-        <Flex
-          display={{ initial: "none", md: "flex" }}
-          gap={"4"}
-          direction={"row"}
-        >
-          {props.links?.map((link, index) => {
-            return <Text key={index} {...(link as any)} />;
-          })}
+          <Flex
+            display={{ initial: "none", md: "flex" }}
+            gap={"4"}
+            direction={"row"}
+          >
+            {props.links?.map((link, index) => {
+              return <Text key={index} {...(link as any)} />;
+            })}
+          </Flex>
         </Flex>
-      </Flex>
+      </Container>
     </Box>
   );
 }
